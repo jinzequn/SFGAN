06eadf56c90854080c5d28329e376e31011af225
jzeee <jinzequn@hotmail.com>
2017-07-15 19:58:24 +0800

    change model

--- predict.py
+++ predict.py
@@ -8,13 +8,32 @@ learning_rate = 10e-5
 save_step = 500
 
 
-def upscale2d(x):
-    x = x.outputs
-    size = x.get_shape().as_list()
-    x = tf.image.resize_nearest_neighbor(x, (size[1]*2, size[2]*2))
-    x = InputLayer(x, name='upscale_inputs')
-    tl.layers.set_name_reuse(True)
-    return x
+# def upscale2d(x):
+#     x = x.outputs
+#     size = x.get_shape().as_list()
+#     x = tf.image.resize_nearest_neighbor(x, (size[1]*2, size[2]*2))
+#     x = InputLayer(x, name='upscale_inputs')
+#     tl.layers.set_name_reuse(True)
+#     return x
+
+class upscale2d(Layer):
+    def __init__(
+        self,
+        layer = None,
+        name = 'upscale2d',
+    ):
+        Layer.__init__(self, name = name)
+        self.inputs = layer.outputs
+        size = self.inputs.get_shape().as_list()
+        self.inputs = tf.image.resize_nearest_neighbor(self.inputs,(size[1]*2,size[2]*2))
+        self.outputs = self.inputs
+        
+        self.all_layers = list(layer.all_layers)
+        self.all_params = list(layer.all_params)
+        self.all_drop = dict(layer.all_drop)
+        self.all_layers.extend( [self.outputs] )
+        
+
 
 
 def data_loader(filename):
@@ -53,35 +72,31 @@ def Encoder(Inputs, is_train=True, reuse=None):
         tl.layers.set_name_reuse(reuse)
         n = InputLayer(Inputs, name='in_E')
         #256x256
-        net_c16 = Conv2d(n, 16, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c16 = Conv2d(n, 16, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                    b_init=b_init, name='g_c1')
         net_bn1 = BatchNormLayer(net_c16, act=tf.nn.relu, gamma_init=g_init, is_train=is_train, name='g_bn1')
         #256x256
-        net_c32 = Conv2d(net_bn1, 32, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c32 = Conv2d(net_bn1, 32, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                    b_init=b_init, name='g_c2')
         net_bn2 = BatchNormLayer(net_c32, act=tf.identity, gamma_init=g_init, is_train=is_train, name='g_bn2')
-        net_pool1 = MaxPool2d(net_bn2,filter_size=(2, 2), name='g_pool1')
         #128x128
-        net_c64 = Conv2d(net_pool1, 64, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c64 = Conv2d(net_bn2, 64, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                    b_init=b_init, name='g_c3')
         net_bn3 = BatchNormLayer(net_c64, act=tf.identity, gamma_init=g_init, is_train=is_train, name='g_bn3')
         #128x128
-        net_c128 = Conv2d(net_bn3, 128, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c128 = Conv2d(net_bn3, 128, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='g_c4')
         net_bn4 = BatchNormLayer(net_c128, act=tf.identity, gamma_init=g_init, is_train=is_train, name='g_bn4')
-        net_pool2 = MaxPool2d(net_bn4, filter_size=(2, 2), name='g_pool2')
         #64x64
-        net_c256 = Conv2d(net_pool2, 256, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c256 = Conv2d(net_bn4, 256, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='g_c5')
         net_bn5 = BatchNormLayer(net_c256, act=tf.identity, gamma_init=g_init, is_train=is_train, name='g_bn5')
-        net_pool3 = MaxPool2d(net_bn5, filter_size=(2, 2), name='g_pool3')
         #32x32
-        net_c512 = Conv2d(net_pool3, 512, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c512 = Conv2d(net_bn5, 512, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='g_c6')
         net_bn6 = BatchNormLayer(net_c512, act=tf.identity, gamma_init=g_init, is_train=is_train, name='g_bn6')
-        net_pool4 = MaxPool2d(net_bn6, filter_size=(2, 2), name='g_pool4')
         #16x16
-        net_c512 = Conv2d(net_pool4, 512, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c512 = Conv2d(net_bn6, 512, (4, 4), (2, 2), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='g_c7')
     # variables = tf.contrib.framework.get_variables(vs)
     logits = net_c512.outputs
@@ -109,39 +124,35 @@ def Decoder(inputs, is_train=True, reuse=None):
         # 16x16
         net_c1 = Conv2d(n, 512, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                    b_init=b_init, name='d_c1')
-        net_bn1 = BatchNormLayer(net_c1, act=tf.identity, gamma_init=g_init, is_train=is_train, name='d_bn1')
-        net_upsca1 = upscale2d(net_bn1)
+        net_upsca1 = upscale2d(net_c1,name= 'net_upsca1')
         #32x32
         net_c2 = Conv2d(net_upsca1, 512, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                    b_init=b_init, name='d_c2')
-        net_bn2 = BatchNormLayer(net_c2, act=tf.identity, gamma_init=g_init, is_train=is_train, name='d_bn2')
-        net_upsca2 = upscale2d(net_bn2)
+        net_upsca2 = upscale2d(net_c2,name= 'net_upsca2')
         # 64x64
         net_c3 = Conv2d(net_upsca2, 256, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                    b_init=b_init, name='d_c3')
-        net_bn3 = BatchNormLayer(net_c3, act=tf.identity, gamma_init=g_init, is_train=is_train, name='d_bn3')
-        net_upsca3 = upscale2d(net_bn3)
+        net_upsca3 = upscale2d(net_c3,name= 'net_upsca3')
         # 128x128
         net_c4 = Conv2d(net_upsca3, 128, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='d_c4')
-        net_bn4 = BatchNormLayer(net_c4, act=tf.identity, gamma_init=g_init, is_train=is_train, name='d_bn4')
+        net_upsca4 = upscale2d(net_c4,name= 'net_upsca4')
         # 128x128
-        net_c5 = Conv2d(net_bn4, 64, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c5 = Conv2d(net_upsca4, 64, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='d_c5')
-        net_bn5 = BatchNormLayer(net_c5, act=tf.identity, gamma_init=g_init, is_train=is_train, name='d_bn5')
-        net_upsca4 = upscale2d(net_bn5)
+        net_upsca5 = upscale2d(net_c5,name= 'net_upsca5')
         # 256x256
-        net_c6 = Conv2d(net_upsca4, 32, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
+        net_c6 = Conv2d(net_upsca5, 32, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='d_c6')
-        net_bn6 = BatchNormLayer(net_c6, act=tf.identity, gamma_init=g_init, is_train=is_train, name='d_bn6')
-
+        net_upsca6 = upscale2d(net_c6,name= 'net_upsca6')
         # 256x256
-        net_c512 = Conv2d(net_bn6, 3, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init,
+        net_c7 = Conv2d(net_upsca6, 3, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init,
                                     b_init=b_init, name='d_c7')
+        net_upsca7 = upscale2d(net_c7,name= 'net_upsca7')
 
     # variables = tf.contrib.framework.get_variables(vs)
-    logits = net_c512.outputs
-    output = net_c512
+    logits = net_upsca7.outputs
+    output = net_upsca7
     return output, logits
 
 
@@ -167,7 +178,7 @@ def Discriminator(inputs, reuse=False):
 
 def main():
     iter_counter = 0
-    real_image_data, label_data = data_loader('train.tfrecords')
+    real_image_data, label_data = data_loader('test.tfrecords')
     img_batch, label_batch = tf.train.shuffle_batch([real_image_data, label_data],
                                                     batch_size=batch_size,
                                                     capacity=200,
@@ -178,19 +189,18 @@ def main():
 
     #==============================MODEL=====================================
 
-    Enc_z_fake, Enc_logits_fake = Encoder(img_batch, is_train=True, reuse=False)
-    Enc_z_real, Enc_logits_real = Encoder(img_batch, is_train=False, reuse=True)
 
-    Dis_z_fake, Dis_logits_fake = Discriminator(Enc_logits_fake, reuse=False)
-    Dis_z_real, Dis_logits_real = Discriminator(Enc_logits_real, reuse=True)
+    Enc_z, Enc_logits = Encoder(img_batch, is_train=True, reuse=False)
+    # Enc_z_real, Enc_logits_real = Encoder(img_batch, is_train=False, reuse=True)
 
-    Dec_z, Dec_logits = Decoder(Enc_logits_fake, is_train=True, reuse=False)
+    Dis_z_fake, Dis_logits_fake = Discriminator(Enc_logits, reuse=False)
+
+    Dec_z, Dec_logits = Decoder(Enc_logits, is_train=True, reuse=False)
 
     Dis_loss_fake = tl.cost.sigmoid_cross_entropy(Dis_logits_fake, label_batch, name='discriminator_loss_fake')
-    Dis_loss_real = tl.cost.sigmoid_cross_entropy(Dis_logits_real, label_batch, name='discriminator_loss_real')
-    Dis_loss = Dis_loss_real-Dis_loss_fake
+    Dis_loss = Dis_loss_fake
 
-    Dec_loss = tl.cost.mean_squared_error(Dec_logits, img_batch)
+    Dec_loss = tl.cost.mean_squared_error(Dec_logits, img_batch)+0.5*tl.cost.sigmoid_cross_entropy(Dis_logits_fake,1-label_batch,name=None)
 
     Dec_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.5).minimize(Dec_loss)
     Dis_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.5).minimize(Dis_loss)
@@ -198,30 +208,18 @@ def main():
     # ===============================TRAIN=====================================================
 
     with tf.Session() as sess:
-        sess.run(tf.local_variables_initializer())
-        sess.run(tf.global_variables_initializer())
+        tl.layers.initialize_global_variables(sess)
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
-            
-        for i in range(epoch):
-            print('epoch %d' % i)
-            errDis, _ = sess.run([Dis_loss, Dis_optimizer])
-            for _ in range(2):
-                errDec, _ = sess.run([Dec_loss, Dec_optimizer])
-            print('errDis:', errDis)
-            print('errDec:', errDec)
+        load_enc_params = tl.files.load_npz(name='Enc_model.npz')
+        load_dec_params = tl.files.load_npz(name='Dec_model.npz')
+        tl.files.assign_params(sess, load_dec_params, Dec_z)
+        tl.files.assign_params(sess, load_enc_params, Enc_z)
 
-            iter_counter += 1
-            if np.mod(iter_counter, save_step) == 0:
-                img = sess.run([Dec_logits])
-                img = img[0]
-                tl.visualize.save_images(img, [1,1], './train_{:02d}_{:04d}.png'.format(epoch,i))
-                print('image saved!')
-                
-            if np.mod(iter_counter, save_step) == 0:
-                tl.files.save_npz(Enc_z_fake.all_params, name='Enc_model.npz')
-                tl.files.save_npz(Dec_z.all_params, name='Dec_model.npz')
-                print('model saved!')
+        img = sess.run([Dec_logits])
+        img = img[0]
+        tl.visualize.save_images(img, [1,1], 'predict.png')
+        print('predict done !')
 
         coord.request_stop()
         coord.join(threads)
@@ -229,4 +227,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    n = tl.layers.DeConv2d
